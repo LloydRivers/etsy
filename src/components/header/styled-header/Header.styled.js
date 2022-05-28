3ffc40e30927912cc859d2379a045733883963d4
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-05-28 17:01:45 +0100

    style input

--- src/components/header/styled-header/Header.styled.js
+++ src/components/header/styled-header/Header.styled.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-`
-
-
-
-
+  display: flex;
+`;
+export const Input = styled.input`
+  border-width: 2px;
+  border-color: #222222;
+  box-sizing: border-box;
+`;
